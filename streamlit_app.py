edf223917e30d2d278bee499c921f9f700c72fce
Jean Miranda <156374467+JeanJMH@users.noreply.github.com>
2024-11-02 23:12:53 -0400

    First template.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,29 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableBranch
 import langchain
 
-
-
-
-st.title("🎈 My new app")
+st.title("Share with us your experience of the latest trip")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+llm = OpenAI(openai_api_key=openai_api_key)
+
+feedback_type_template = """You are team support analyst. Analyze the following feedback text to determine if it inside the following clasification:
+1. Negative experience: for negative experiences caused by the airline's fault (for example lost luggage).
+2. Negative beyond control: for negative experiences beyond the airline's control (for example weather-related delays).
+3. Positive experience: for positive experiences.
+
+Respond only the clasification
+
+Text:
+{feedback}
+"""
+### Create the decision-making chain
+
+feedback_type_chain = (
+    PromptTemplate.from_template(feedback_type_template)
+    | llm
+    | StrOutputParser()
+)
+
+
